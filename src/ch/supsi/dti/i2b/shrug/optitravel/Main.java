7cbf10f4cebb5aa0bfc60b4525f7885b9727d11e
Denys Vitali <denys@denv.it>
2018-02-25 19:03:24 +0100

    Revert to Java standalone application

--- src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
@@ -1,12 +1,7 @@
 package ch.supsi.dti.i2b.shrug.optitravel;
-import spark.Spark;
-
-import static spark.Spark.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Spark.staticFileLocation("/public"); // Static files
-        get("/", (req, res) -> "Hello World");
+        System.out.println("Hi.");
     }
 }
