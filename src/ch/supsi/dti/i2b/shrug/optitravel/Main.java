d1f019e00ea4babafe8cc5092c92b35cd2f49c33
Denys Vitali <denys@denv.it>
2018-02-27 19:10:54 +0100

    Add JavaFX

--- src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
@@ -1,7 +1,19 @@
 package ch.supsi.dti.i2b.shrug.optitravel;
-public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hi.");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setTitle("OptiTravel");
+        stage.setScene(scene);
+        stage.show();
     }
 }
