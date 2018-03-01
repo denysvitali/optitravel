79c2fec8d5956fc3f5bbf87c6878924a078a1760
Denys Vitali <denys@denv.it>
2018-03-01 19:25:33 +0100

    Switch to GMapsFX

--- src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
@@ -1,11 +1,11 @@
 package ch.supsi.dti.i2b.shrug.optitravel;
 
-import ch.supsi.dti.i2b.shrug.optitravel.api.MapQuest.MapQuestWrapper;
-import ch.supsi.dti.i2b.shrug.optitravel.api.MapQuest.MapType;
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -20,12 +20,44 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
 
-        MapQuestWrapper mapQuestWrapper = new MapQuestWrapper();
+        /*MapQuestWrapper mapQuestWrapper = new MapQuestWrapper();
         Image mapImage = mapQuestWrapper.getMapImage(46.0037, 8.9511, 17, MapType.HYB);
 
         ImageView imageView = new ImageView();
         imageView.setImage(mapImage);
+        root.getChildren().add(imageView);*/
 
-        root.getChildren().add(imageView);
+        //Marker marker1 = new Marker.createProvided(new Marker.Provided("./"));
+
+
+        /*MapView mapView = new MapView();
+        mapView.setCenter(new Coordinate(46.0037, 8.9511));
+        mapView.setZoom(17.0);
+        mapView.setBingMapsApiKey("Ar02eGwO-GorbA5RsvRrxhDWJkY7_J5980FqcGFyTjWiBI2BFMJ254TMJ_MzbWyX");
+        mapView.setMapType(MapType.BINGMAPS_ROAD);
+        mapView.initialize();*/
+
+        //mapView.addMarker(marker1);
+
+
+
+        GoogleMapView mapView = new GoogleMapView();
+        //mapView.setKey("AIzaSyAvtzzsAPAlOrK8JbGfXfHMt18MbqCqrj4");
+        mapView.addMapInializedListener(() -> {
+            MapOptions mapOptions = new MapOptions();
+
+            mapOptions.center(new LatLong(46.0037, 8.9511))
+                    .mapType(MapTypeIdEnum.ROADMAP)
+                    .overviewMapControl(false)
+                    .panControl(false)
+                    .rotateControl(false)
+                    .scaleControl(false)
+                    .streetViewControl(false)
+                    .zoomControl(false)
+                    .zoom(12);
+            mapView.createMap(mapOptions);
+        });
+        root.getChildren().add(mapView);
+
     }
 }
