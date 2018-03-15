a96fad570ef1bbb5ffffc1b988249ad62dad47bb
Filippo <purafilippo@gmail.com>
2018-03-15 09:21:41 +0100

    Restored initial

--- src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
@@ -1,6 +1,12 @@
 package ch.supsi.dti.i2b.shrug.optitravel;
 
 import ch.supsi.dti.i2b.shrug.optitravel.api.MapQuest.MapQuestWrapper;
+import ch.supsi.dti.i2b.shrug.optitravel.api.TransitLand.TransitLandWrapper;
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -8,8 +14,17 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+
+import java.io.IOException;
+
 public class Main extends Application {
 
+ /*   public static void main(String [ ] args) throws IOException {
+        TransitLandWrapper tr = new TransitLandWrapper();
+
+
+        tr.getNearbyStops(46.0037,8.9511);
+    }*/
     @Override
     public void start(Stage stage) throws Exception {
         StackPane root = new StackPane();
@@ -19,6 +34,8 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
 
+
+
         MapQuestWrapper mapQuestWrapper = new MapQuestWrapper();
         Image mapImage = mapQuestWrapper.getMapImage(46.0037, 8.9511, 7);
 
@@ -26,5 +43,29 @@ public class Main extends Application {
         imageView.setImage(mapImage);
 
         root.getChildren().add(imageView);
+
+
+
+        GoogleMapView mapView = new GoogleMapView();
+        mapView.addMapInializedListener(() -> {
+            LatLong lugano_location = new LatLong(46.0037, 8.9511);
+            MapOptions mapOptions = new MapOptions();
+
+            mapOptions.center(lugano_location)
+                    .mapType(MapTypeIdEnum.ROADMAP)
+                    .overviewMapControl(false)
+                    .panControl(false)
+                    .rotateControl(false)
+                    .scaleControl(false)
+                    .streetViewControl(false)
+                    .zoomControl(false)
+                    .zoom(12);
+            mapView.createMap(mapOptions);
+
+
+        });
+        root.getChildren().add(mapView);
+
+
     }
 }
