bbf2b5006a469e993294aa0086faccbb2d105cb8
Filippo <purafilippo@gmail.com>
2018-03-15 21:38:45 +0100

    Completed the transitland implementation of the basic functions

---
+++ src/ch/supsi/dti/i2b/shrug/optitravel/api/TransitLand/results/OperatorsResult.java
@@ -0,0 +1,17 @@
+package ch.supsi.dti.i2b.shrug.optitravel.api.TransitLand.results;
+
+import ch.supsi.dti.i2b.shrug.optitravel.api.TransitLand.Operator;
+
+import java.util.ArrayList;
+
+public class OperatorsResult {
+    private ArrayList<Operator> operators;
+
+    public OperatorsResult(){
+
+    }
+
+    public ArrayList<Operator> getOperators() {
+        return operators;
+    }
+}
