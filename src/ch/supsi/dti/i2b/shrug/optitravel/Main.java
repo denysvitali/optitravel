e4a1ec4bb71d74d94ef49714a6909bb09dd94c56
Denys Vitali <denys@denv.it>
2018-03-18 23:01:51 +0100

    gtfsrs visualization test

--- src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
@@ -8,9 +8,9 @@ import ch.supsi.dti.i2b.shrug.optitravel.api.TransitLand.GPSCoordinates;
 import ch.supsi.dti.i2b.shrug.optitravel.api.TransitLand.TransitLandAPIError;
 import ch.supsi.dti.i2b.shrug.optitravel.api.TransitLand.TransitLandAPIWrapper;
 import com.lynden.gmapsfx.GoogleMapView;
-import com.lynden.gmapsfx.javascript.object.LatLong;
-import com.lynden.gmapsfx.javascript.object.MapOptions;
-import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.lynden.gmapsfx.javascript.object.*;
+import com.lynden.gmapsfx.shapes.Polyline;
+import com.lynden.gmapsfx.shapes.PolylineOptions;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -51,13 +51,15 @@ public class Main extends Application {
         //mapView.setKey("AIzaSyAvtzzsAPAlOrK8JbGfXfHMt18MbqCqrj4");
 
         GTFSrsWrapper gtfsrs = new GTFSrsWrapper();
+        List<Stop> stops;
         try {
-            List<Stop> stops = gtfsrs.getStopsByTrip("t-6ebb8e-lamone-cadempinostazione");
+            stops = gtfsrs.getStopsByTrip("t-4840d5-domodossolastazione");
             for(Stop s : stops){
                 System.out.println(String.format("%s - %s", s.getUid(), s.getName()));
             }
         } catch (GTFSrsError gtfSrsError) {
             gtfSrsError.printStackTrace();
+            return;
         }
 
         mapView.addMapInializedListener(() -> {
@@ -82,19 +84,72 @@ public class Main extends Application {
                     .zoom(12);
             mapView.createMap(mapOptions);
 
-            /*for(Stop s : stops){
-                MarkerOptions markerOptions1 = new MarkerOptions();
+            LatLongBounds bounds = new LatLongBounds();
+
+            for(Stop s : stops){
+                /*MarkerOptions markerOptions1 = new MarkerOptions();
                 markerOptions1.position(
                         new LatLong(
-                                s.getCoordinates().getLatitude(),
-                                s.getCoordinates().getLongitude()
+                                s.getLat(),
+                                s.getLng()
                         )
                 );
                 markerOptions1.animation(Animation.DROP);
                 Marker marker = new Marker(markerOptions1);
-                mapView.getMap().addMarker(marker);
-            }*/
+                mapView.getMap().addMarker(marker);*/
+                bounds.extend(new LatLong(
+                        s.getLat(),
+                        s.getLng()
+                ));
+            }
 
+            MarkerOptions markerOptions1 = new MarkerOptions();
+            markerOptions1.position(
+                    new LatLong(
+                            stops.get(stops.size()-1).getLat(),
+                            stops.get(stops.size()-1).getLng()
+                    )
+            );
+            markerOptions1.animation(Animation.DROP);
+            Marker marker = new Marker(markerOptions1);
+            mapView.getMap().addMarker(marker);
+
+
+            MVCArray mvcArray;
+            LatLong[] ary = new LatLong[stops.size()];
+
+            for (int i = 0; i < stops.size(); i++) {
+                ary[i] = new LatLong(stops.get(i).getLat(), stops.get(i).getLng());
+            }
+
+            mapView.getMap().setCenter(ary[0]);
+
+            mvcArray = new MVCArray(ary);
+
+
+            PolylineOptions poly_opts = new PolylineOptions()
+                    .path(mvcArray)
+                    .strokeColor("#f1c40f")
+                    .strokeWeight(4)
+                    .zIndex(2);
+
+            PolylineOptions poly_stroke_opts = new PolylineOptions()
+                    .path(mvcArray)
+                    .strokeColor("#999")
+                    .strokeWeight(6)
+                    .strokeOpacity(1)
+                    .zIndex(1);
+
+
+            Polyline poly = new Polyline(poly_opts);
+            Polyline poly_stroke = new Polyline(poly_stroke_opts);
+
+            mapView.getMap().addMapShape(poly);
+            mapView.getMap().addMapShape(poly_stroke);
+            mapView.getMap().fitBounds(bounds);
+
+
+            mapView.getMap().panToBounds(bounds);
 
         });
         root.getChildren().add(mapView);
