f341fc31fc3941077c5ed2992b252aa8eea945dd
Denys Vitali <denys@denv.it>
2018-03-18 20:47:01 +0100

    Add tests, switch to double for lat. lng. representationi in GTFS Stop model

--- src/ch/supsi/dti/i2b/shrug/optitravel/api/GTFS_rs/models/Stop.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/api/GTFS_rs/models/Stop.java
@@ -3,24 +3,28 @@ package ch.supsi.dti.i2b.shrug.optitravel.api.GTFS_rs.models;
 public class Stop {
     private String uid;
     private String name;
-    private float lat;
-    private float lng;
-    private String type;
+    private double lat;
+    private double lng;
+    private int location_type;
     private String parent_station;
 
     public String getName() {
         return this.name;
     }
 
-    public float getLat() {
+    public double getLat() {
         return lat;
     }
 
-    public float getLng() {
+    public double getLng() {
         return lng;
     }
 
     public String getUid() {
         return uid;
+    }
+
+    public int getType() {
+        return location_type;
     }
 }
---
+++ test/ch/supsi/dti/i2b/shrug/optitravel/api/GTFS_rs/GTFSrsTest.java
@@ -0,0 +1,49 @@
+package ch.supsi.dti.i2b.shrug.optitravel.api.GTFS_rs;
+
+import ch.supsi.dti.i2b.shrug.optitravel.api.GTFS_rs.models.Stop;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GTFSrsTest {
+    private GTFSrsWrapper gtfSrsWrapper;
+
+    public GTFSrsTest(){
+        gtfSrsWrapper = new GTFSrsWrapper();
+    }
+
+    @Test
+    void StopsByTripId(){
+        String trip_id = "t-6ebb8e-lamone-cadempinostazione";
+
+        assertNotEquals(null, gtfSrsWrapper);
+        List<Stop> stops;
+        try {
+             stops = gtfSrsWrapper.getStopsByTrip(trip_id);
+        } catch (GTFSrsError gtfSrsError) {
+            fail(gtfSrsError);
+            return;
+        }
+
+        assertEquals(9, stops.size());
+        for(Stop s : stops){
+            assertNotEquals(null, s);
+        }
+
+        for(Stop s : stops){
+            assertNotEquals(null, s.getUid());
+            assertNotEquals(null, s.getName());
+            assertNotEquals(null, s.getLat());
+            assertNotEquals(null, s.getLng());
+        }
+
+        assertEquals("Bioggio Molinazzo, Stazione", stops.get(0).getName());
+        assertEquals(46.013046, stops.get(0).getLat());
+        assertEquals(8.915061, stops.get(0).getLng());
+        assertEquals(0, stops.get(0).getType());
+
+
+    }
+}
