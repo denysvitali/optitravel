191d3c7c3092d717933160f743c031404206b868
raenilol <raenilol@gmail.com>
2018-04-12 11:42:48 +0200

    Styling text fields and added date and time controls.

--- src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
@@ -1,22 +1,12 @@
 package ch.supsi.dti.i2b.shrug.optitravel;
 
-import com.lynden.gmapsfx.GoogleMapView;
-import com.lynden.gmapsfx.MapComponentInitializedListener;
-import com.lynden.gmapsfx.javascript.object.LatLong;
-import com.lynden.gmapsfx.javascript.object.MapOptions;
-import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    @FXML
-    private GoogleMapView mapView;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/main.fxml"));
--- src/ch/supsi/dti/i2b/shrug/optitravel/ui/MainController.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/ui/MainController.java
@@ -1,5 +1,7 @@
 package ch.supsi.dti.i2b.shrug.optitravel.ui;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
 import com.lynden.gmapsfx.javascript.object.GoogleMap;
@@ -8,28 +10,27 @@ import com.lynden.gmapsfx.javascript.object.MapOptions;
 import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
 import javafx.fxml.FXML;
 
-public class MainController implements MapComponentInitializedListener {
+public class MainController {
 
+    @FXML
+    private JFXTextField tfEndPoint;
+    @FXML
+    private JFXComboBox cbTripPeriod;
+    @FXML
+    private JFXTextField tfStartPoint;
     @FXML
     private GoogleMapView mapView;
 
+
+    private MapController mapController;
+
     @FXML
     private void initialize() {
-        mapView.addMapInializedListener(this);
+        mapController = new MapController(mapView);
+        mapView.addMapInializedListener(mapController);
+
+        cbTripPeriod.getSelectionModel().selectFirst();
     }
 
-    @Override
-    public void mapInitialized() {
 
-        MapOptions options = new MapOptions();
-        options.mapType(MapTypeIdEnum.ROADMAP)
-                .mapTypeControl(false)
-                .center(new LatLong(-50.6071131, 165.9725615))
-                .zoom(13)
-                .streetViewControl(false)
-                .mapTypeControl(false);
-        mapView.setKey("AIzaSyAvtzzsAPAlOrK8JbGfXfHMt18MbqCqrj4");
-
-        GoogleMap map = mapView.createMap(options);
-    }
 }
