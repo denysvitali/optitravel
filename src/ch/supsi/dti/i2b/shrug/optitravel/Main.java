101d376bd1680409663860d8a23f32f28fbe1e40
Raenilol <raenilol@gmail.com>
2018-05-10 13:33:47 +0200

    Added options to trip timeframe combobox and hide/show for date/time pickers

--- src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/Main.java
@@ -6,21 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.ResourceBundle;
-
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        ResourceBundle locale = ResourceBundle.getBundle("locale/strings");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/main.fxml"));
         Parent root = loader.load();
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 1280, 720);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("optitravel");
         primaryStage.setMinWidth(800);
         primaryStage.setMinHeight(600);
-        primaryStage.setWidth(1280);
-        primaryStage.setHeight(720);
+        primaryStage.setTitle("Optitravel");
         primaryStage.show();
     }
 }
--- src/ch/supsi/dti/i2b/shrug/optitravel/ui/MainController.java
+++ src/ch/supsi/dti/i2b/shrug/optitravel/ui/MainController.java
@@ -1,34 +1,36 @@
 package ch.supsi.dti.i2b.shrug.optitravel.ui;
 
 import ch.supsi.dti.i2b.shrug.optitravel.api.GoogleMaps.GeocodingWrapper;
-import ch.supsi.dti.i2b.shrug.optitravel.api.GoogleMaps.model.Place;
-import ch.supsi.dti.i2b.shrug.optitravel.api.TransitLand.Callback;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXDecorator;
 import com.jfoenix.controls.JFXTimePicker;
 import com.lynden.gmapsfx.GoogleMapView;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-
-import java.util.List;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.HBox;
 
 public class MainController {
 
     @FXML
-    public JFXTimePicker tpTime;
+    private JFXTimePicker tpTime;
     @FXML
-    public JFXDatePicker dpDate;
+    private JFXDatePicker dpDate;
     @FXML
-    public JFXButton fabSend;
+    private JFXButton fabSend;
     @FXML
-    private ComboBox cbTripPeriod;
+    private ComboBox<TimeFrame> cbTripPeriod;
     @FXML
     private AutoCompleteTextField tfStartPoint;
     @FXML
     private AutoCompleteTextField tfEndPoint;
     @FXML
     private GoogleMapView mapView;
+    @FXML
+    private HBox routeOptionsContainer;
 
     private MapController mapController;
 
@@ -38,31 +40,57 @@ public class MainController {
     @FXML
     private void initialize() {
 
-        tfStartPoint.setOnSelect((e) -> {
-            GeocodingWrapper.getPlacesAsync(tfStartPoint.getSelectedEntry().getDescription(), new Callback<List<Place>>() {
-                @Override
-                public void exec(List<Place> places) {
-                    Platform.runLater(() -> mapController.addMarker(places.get(0).getGeometry().getLocation(), MapController.NodeType.ORIGIN));
-                }
-            });
-        });
-        tfEndPoint.setOnSelect((e) -> {
-            GeocodingWrapper.getPlacesAsync(tfEndPoint.getSelectedEntry().getDescription(), new Callback<List<Place>>() {
-                @Override
-                public void exec(List<Place> places) {
-                    Platform.runLater(() -> mapController.addMarker(places.get(0).getGeometry().getLocation(), MapController.NodeType.DESTINAION));
-                }
-            });
-        });
+        // Setup autocompletion on origin and destination text fields
+        tfStartPoint.setOnSelect((e) ->
+                GeocodingWrapper.getPlacesAsync(tfStartPoint.getSelectedEntry().getDescription(),
+                        places -> Platform.runLater(() -> mapController.addMarker(places.get(0).getGeometry().getLocation(), MapController.NodeType.ORIGIN))));
+        tfEndPoint.setOnSelect((e) ->
+                GeocodingWrapper.getPlacesAsync(tfEndPoint.getSelectedEntry().getDescription(),
+                        places -> Platform.runLater(() -> mapController.addMarker(places.get(0).getGeometry().getLocation(), MapController.NodeType.DESTINAION))));
 
+        // Initialise map
         mapController = new MapController(mapView, this);
         mapView.addMapInializedListener(mapController);
 
+        // Setup trip period selection combobox
+        cbTripPeriod.getSelectionModel().selectFirst();
+        cbTripPeriod.getItems().addAll(TimeFrame.LEAVE_NOW, TimeFrame.DEPART_AT, TimeFrame.ARRIVE_BY);
+        // Set date and time pickers to hide when combobox is set to "leave now"
+        cbTripPeriod.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            switch (newValue) {
+                case LEAVE_NOW:
+                    tpTime.setManaged(false);
+                    dpDate.setManaged(false);
+                    tpTime.setVisible(false);
+                    dpDate.setVisible(false);
+                    break;
+                default:
+                    tpTime.setManaged(true);
+                    dpDate.setManaged(true);
+                    tpTime.setVisible(true);
+                    dpDate.setVisible(true);
+                    break;
+            }
+        });
         cbTripPeriod.getSelectionModel().selectFirst();
 
+        // Set pickers to open when clicking on text field
         dpDate.getEditor().setOnMouseClicked(e -> dpDate.show());
         tpTime.getEditor().setOnMouseClicked(e -> tpTime.show());
         tpTime.setIs24HourView(true);
+        }
+}
 
+enum TimeFrame {
+    LEAVE_NOW("Leave now"), DEPART_AT("Depart at"), ARRIVE_BY("Arrive by");
+    String str;
+
+    TimeFrame(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public String toString() {
+        return str;
     }
 }
