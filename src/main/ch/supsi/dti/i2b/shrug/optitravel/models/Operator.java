e0050acbfea3739e917fd41a5590bca2217c3d6c
raenilol <raenilol@gmail.com>
2018-06-05 11:27:58 +0200

    apoijefeseaiod

--- src/main/ch/supsi/dti/i2b/shrug/optitravel/models/Operator.java
+++
@@ -1,6 +0,0 @@
-package ch.supsi.dti.i2b.shrug.optitravel.models;
-
-public abstract class Operator {
-    public abstract String getName();
-    public abstract String getURL();
-}
--- src/main/ch/supsi/dti/i2b/shrug/optitravel/models/Trip.java
+++
@@ -1,8 +0,0 @@
-package ch.supsi.dti.i2b.shrug.optitravel.models;
-
-import java.util.List;
-
-public abstract class Trip {
-	public abstract List<StopTrip> getStopTrip();
-	public abstract Route getRoute();
-}
