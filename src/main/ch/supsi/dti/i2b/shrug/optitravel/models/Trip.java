3f0ca981479bcfc6d880dc446c7a6fc126e4c1ec
Denys Vitali <denys@denv.it>
2018-06-05 16:33:05 +0200

    WIP: Routing rewriting

--- src/main/ch/supsi/dti/i2b/shrug/optitravel/models/Trip.java
+++ src/main/ch/supsi/dti/i2b/shrug/optitravel/models/Trip.java
@@ -5,4 +5,19 @@ import java.util.List;
 public abstract class Trip {
 	public abstract List<StopTrip> getStopTrip();
 	public abstract Route getRoute();
+
+	public int getStopIndex(Stop s){
+		List<StopTrip> stopTrips = getStopTrip();
+		if(stopTrips == null){
+			return -1;
+		}
+
+		for(StopTrip st : stopTrips){
+			if(st.getStop().equals(s)){
+				return stopTrips.indexOf(st);
+			}
+		}
+
+		return -1;
+	}
 }
