f5ae0cd032599075c4653e4cbeaa034acad5f9a9
Denys Vitali <denys@denv.it>
2018-06-10 22:30:56 +0200

    v1.0.0 (Beta)

--- src/main/ch/supsi/dti/i2b/shrug/optitravel/config/BuildConfig.java
+++ src/main/ch/supsi/dti/i2b/shrug/optitravel/config/BuildConfig.java
@@ -1,10 +1,10 @@
 package ch.supsi.dti.i2b.shrug.optitravel.config;
 
 public class BuildConfig {
-    private static BuildFlavour buildConfig = BuildFlavour.DEV;
+    private static BuildFlavour buildConfig = BuildFlavour.BETA;
     public static final boolean USE_GTFS_REMOTE = true;
-    private static int MAJOR = 0;
-    private static int MINOR = 1;
+    private static int MAJOR = 1;
+    private static int MINOR = 0;
     private static int PATCH = 0;
 
     public static String getVersion(){
